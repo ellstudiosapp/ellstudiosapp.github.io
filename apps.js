224d0277d895e7f8d88c5a7839bd0984f7565672
ellstudiosapp <ellstudiosapp@gmail.com>
2026-05-16 20:35:14 +0700

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,66 @@
+const appsData = {
+  developer: "Ellstudiosapp",
+  total_apps: 20,
+  applications: [
+    {
+      id: 1,
+      app_name: "Soal CPNS - Latihan CAT",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.soalcpns",
+      image_url: "https://play-lh.googleusercontent.com/S58ngOEh7UdZiZRIhon5ZluWL23sh739RnIgCf9ixyOFrzZHAIY4IB051UUIrAkktEs=s256"
+    },
+    {
+      id: 3,
+      app_name: "Soal PPPK - Latihan CAT",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.soalpppk",
+      image_url: "https://play-lh.googleusercontent.com/AcbJHuhMego-MzSUWRGDLqfovRaT2fb72b4h2rfXU7fGwjk7ZHFNiJKeFWoOKTyArT4=s256"
+    },
+    {
+      id: 4,
+      app_name: "Soal PPPK Kemenag",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.pppkkemenag",
+      image_url: "https://play-lh.googleusercontent.com/gqbomqHmuTqKHZbroKUhog3dv9NI6VdfOPOa-TgZ5i7ZKS_HmZhRBrN1L1oV6lx6Fb8S=s256"
+    },
+    {
+      id: 5,
+      app_name: "Soal UTBK - Latihan SNBT",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.soalutbk",
+      image_url: "https://play-lh.googleusercontent.com/kiNjCGBwyFGnu8y97DUGDDuvTd5yEO58PVGW44iV2C3xj00PqgQ5Ff5AIBkdfd8861U=s256"
+    },
+    {
+      id: 2,
+      app_name: "Soal CPNS - Simulasi CAT",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.cpnstwktiutkp",
+      image_url: "https://play-lh.googleusercontent.com/ddAnFlarIsYVu_yAnEsPPQR2ZTNBM4h8jlz8OegwXpCihGym_fevTBMNq1VEwNisYbQiAQMAchxHpW2tmydr=s256"
+    },
+    {
+      id: 6,
+      app_name: "Belajar Bahasa Inggris Pemula",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.bahasainggrispemulapanduan",
+      image_url: "https://play-lh.googleusercontent.com/LDRTJUDtP9QGYfoCEShenEh9C1de7pQbUDWZ0nywfKN68sRH3TF_BKwJynztbyMDkNy6TAwVFJVHh6Fb82VD=s256"
+    },
+    {
+      id: 7,
+      app_name: "Fluent English",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.fluent_english",
+      image_url: "https://play-lh.googleusercontent.com/4iTZvuOhP7PbYD2ur9zyJYThCzOmOF-AowqXh9rlCl8oS3ik4Bg73eBt85_QQQfUgjr8rdKNnpVA6LA9m9yIpw=s256"
+    },
+    {
+      id: 8,
+      app_name: "Kamus Bahasa Indonesia (KBBI)",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.kbbi.kamusbesarbahasaindonesia",
+      image_url: "https://play-lh.googleusercontent.com/a8fd9vpXp13d6OmY5CSBDiPMC6uHKtoJhIlUDMto4FKUscCSc5c169pO6UWc6dAiy50=s256"
+    },
+    {
+      id: 9,
+      app_name: "Kamus Bahasa Lampung (Dialek A & O)",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.kamusbahasalampung_ao",
+      image_url: "https://play-lh.googleusercontent.com/eAzRqVzgUXjjYIRIZZemco3SMLjDwHEL8Skrkc1_Bm3BWFZHss5gttSgyMIQwfJbPJTn=s256"
+    },
+    {
+      id: 10,
+      app_name: "Tabel Perkalian Matematika",
+      play_store_url: "https://play.google.com/store/apps/details?id=com.ellstudiosapp.multiplicationtable",
+      image_url: "https://play-lh.googleusercontent.com/XF2OQD50wxIpnWxIh2nIpqQLycJ0DVt0L2kVLiryEYEZqehJTpR_EgiHp761LyVtrlYG=s256"
+    }
+  ]
+};
